91d7cd743fc6743e1fafd94fe6b14a80390d2e13
Mark <markeasting@gmail.com>
2024-01-07 20:36:50 +0100

    Removed 'assert' since this is now handled by register()

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { ExtendableContainer } from './ExtendableContainer';
 export * from './Container';
 export * from './ExtendableContainer';
 export * from './types';
-export * from './assert';
 export * from './errors';
 
 /**
